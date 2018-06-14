f146e3b1228e650ec724bb8f1694a8c6f386098b
icsl-Jeon <junbs95@gmail.com>
2018-06-14 20:19:15 +0900

    still path making

--- CBBA/scripts/CBBA_Main.py
+++ CBBA/scripts/CBBA_Main.py
@@ -52,3 +52,61 @@ def CBBA_main(agents,tasks,Lt,connectivity_graph):
 
     print ('total score : {}'.format(total_score))
     return cbba
+
+
+def CBBA_solve(task_positions,unit_positions):
+    Nt=len(task_positions)
+    Nu=len(unit_positions)
+
+    agent_list=[], task_list=[]
+    ##############
+    # AGENT LIST #
+    ##############
+
+    # agent list construction
+    for i in range(Nu):
+        # PARSING DATA
+        # position
+        agent_pos_x = unit_positions[i].x
+        agent_pos_y = unit_positions[i].y
+        agent_pos_z = 0.0
+
+        # type
+        agent_type = 0
+
+        agent_unit = agent(agent_pos_x, agent_pos_y, agent_pos_z, idx=i, agent_type=agent_type, fuel=1, nom_vel=2)
+        agent_list.append(agent_unit)
+
+    #############
+    # TASK LIST #
+    #############
+
+    # task list construction
+    for j in range(Nt):
+        # PARSING DATA
+        # position
+        task_pos_x = task_positions[j].x
+        task_pos_y = task_positions[j].y
+        task_pos_z = 0.0
+        task_type = 0 # type
+        value = 100
+        start_t = 0.0
+        end_t = 100.0
+        duration = 1
+        discount = 0.1
+
+        task_unit = task(idx=j, task_type=task_type, x=task_pos_x, y=task_pos_y, z=task_pos_z, start_t=start_t,
+                         end_t=end_t, task_value=100, duration=duration)
+        task_list.append(task_unit)
+
+    # connectivity graph
+    connectivity_graph = np.ones((Nu, Nu)) - np.eye(Nu)
+
+    ##############
+    # CBBA SOLVE #
+    ##############
+
+    cbba = CBBA_main(agent_list, task_list, Lt=10, connectivity_graph=connectivity_graph)
+    return cbba
+
+
--- CBBA/scripts/CBBA_node.py
+++ CBBA/scripts/CBBA_node.py
@@ -3,75 +3,167 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-from keyboard.msg import Key
+from nav_msgs.msg import Path
 from visualization_msgs.msg import *
-import roslib
+from kiro_gui_msgs.msg import PositionArray
+from CBBA.srv import *
+from CBBA_Main import *
+import numpy as np
 import tf
-import geometry_msgs.msg
-
-'''initial flags'''
-
-isReceiving = False
-isFinishing = False
-isCalc = False
+from geometry_msgs.msg import Pose
 
 
-'''marker scale'''
-
+'''flags'''
+isTask=False # have received tasks?
+isAgents=True # have received agents?
+isSolve=False  # have we sovled CBBA
 
 search_pos=[]
 listener=tf.TransformListener()
-tb0_pos=[0,0]
-tb1_pos=[0,0]
-tb2_pos=[0,0]
+
+''' Several global variables '''
 
 # rviz marker
-search_pos_marker_pub=rospy.Publisher('/search_position_markers',MarkerArray,queue_size=1)
-
-'''To use this call back function '''
-'''In Rviz -> tool properties -> 2D Nav goal topic name : /search_position '''
-
-'''call back functions '''
-def click_pnts_callback(data):
-    global isReceiving,isCalc,isFinishing
-    print(isReceiving)
-    if isReceiving:
-        search_pos.append(data.pose.position)
-        rospy.loginfo("received point : [%.4f, %.4f]",data.pose.position.x,
-                      data.pose.position.y)
-    # rospy.loginfo("saved pnts until now:")
-    # for pos in search_pos:
-    #     rospy.loginfo("[%.4f, %.4f, %.4f]",pos.x,pos.y,pos.z)
-
-def key_sub(data):
-    data=data.code
-    global isReceiving, isCalc, isFinishing
-    if data == 119: # renew
-        '''reset all the flags'''
-        isReceiving = False
-        isFinishing = False
-        isCalc = False
-        rospy.loginfo("renewed")
-    elif data == 97: # start receiving points
-        isReceiving = True
-        rospy.loginfo("start receiving points")
-    elif data == 115: # finish receiving points
-        isFinishing = True
-        rospy.loginfo("finish receiving points")
-    elif data == 100: # calculate CBBA algorithm
-        isCalc = True
-        rospy.loginfo("finish receiving points")
-    else :
-        rospy.loginfo("wrong key input")
+search_pos_marker_pub = rospy.Publisher('/search_position_markers', MarkerArray, queue_size=1)
 
 
-'''reg function'''
+##################
+# Agent location #
+##################
+Nu=3
+tb0_pos=[-7,-1]
+tb1_pos=[7,-1]
+tb2_pos=[0.5,3.0]
 
-def point_receiver():
-    rospy.Subscriber("/search_position",PoseStamped,click_pnts_callback)
+agent_pos=[]
 
-def user_input():
-    rospy.Subscriber("/keyboard/keyup",Key,key_sub)
+pose = Pose()
+pose.position.x=tb0_pos[0]; pose.position.y=tb0_pos[1]
+agent_pos.append(pose.position)
+
+pose = Pose()
+pose.position.x=tb1_pos[0]; pose.position.y=tb1_pos[1]
+agent_pos.append(pose.position)
+
+pose = Pose()
+pose.position.x=tb2_pos[0]; pose.position.y=tb2_pos[1]
+agent_pos.append(pose.position)
+
+##############################
+# Path of task for each unit #
+##############################
+
+allocated_paths=[] # array of Navmsg.Path
+
+
+'''CBBA computation '''
+
+def allocate_path():
+    global isTask,isAgents,isSolve, allocated_paths
+    allocated_paths=[]
+    if isTask and isAgents:
+        cbba=CBBA_solve(search_pos,agent_pos)
+        # Path extraction & parsing
+        cbba_path=cbba.path
+
+        # iterate through the agents
+        for i in range(cbba.Nu):
+            # path of this agent
+            path=Path()
+
+            # Firstly, we put the position of agent
+            # TODO how about making a function for this procedure?
+            pose = Pose()
+            pose.position.x=cbba.agent_list[i].x
+            pose.position.y = cbba.agent_list[i].y
+            pose.position.z = cbba.agent_list[i].z
+            path.poses.append(pose)
+
+
+            # Secondly, we put the task position of the path
+            for j in range(cbba.Nt):
+                if not cbba_path[i,j] == -1:
+                    pose=Pose()
+                    pose.position.x = cbba.task_list[cbba_path[i,j]].x
+                    pose.position.y = cbba.task_list[cbba_path[i,j]].y
+                    pose.position.z = cbba.task_list[cbba_path[i,j]].z
+                    path.poses.append(pose)
+
+            allocated_paths.append(path)
+
+        return allocated_paths
+    else:
+        rospy.logwarn('task or agents are not provided')
+        return allocated_paths
+
+
+
+'''search positions callback'''
+def sps_callback(data):
+    global search_pos
+    search_pos=[]
+    rospy.loginfo('received search position array')
+    isTask=True
+    for pos in data.positions:
+        search_pos.append(pos.pose.position)
+
+
+def task_receiver():
+    rospy.Subscriber("/search_position_array",PositionArray,sps_callback)
+
+''' marker pusblish / tf '''
+
+def marker_publish():
+    global search_pos
+     # marker for search positions published
+    pos_idx = 0
+    marker_array = MarkerArray()
+    for pos in search_pos:
+        # visualization marker
+        marker = Marker()
+        marker.type = Marker.CUBE
+        marker.action = Marker.ADD
+        marker.header.frame_id = "map"
+        marker.header.stamp = rospy.Time.now()
+        marker.id = pos_idx
+        marker.lifetime = rospy.Duration()
+        marker.ns = 'search_positions'
+        scale = 0.2
+        marker.scale.x = scale
+        marker.scale.y = scale
+        marker.scale.z = scale
+        marker.pose.orientation.w = 1.0
+        marker.color.r = 1
+        marker.color.g = 0.2
+        marker.color.b = 0.2
+        marker.color.a = 1
+        marker.pose.position = pos
+
+        marker_array.markers.append(marker)
+        # transform
+        pos_name = 'sp' + str(pos_idx)
+        br.sendTransform((pos.x, pos.y, pos.z), (0, 0, 0, 1), rospy.Time.now(), pos_name, 'map')
+        pos_idx += 1
+    search_pos_marker_pub.publish(marker_array)
+
+
+''' extract the tf for state of each robot '''
+# TODO still handling the exception error ....
+def state_receiver():
+    (trans0, rot0) = listener.lookupTransform('tb3_0/odom', 'map', rospy.Time(0))
+    tb0_pos[0] = trans0[0]
+    tb0_pos[1] = trans0[1]
+    # rospy.loginfo('tb0 position: [%.4f, %.4f]', tb0_pos[0], tb0_pos[1])
+    (trans1, rot1) = listener.lookupTransform('tb3_1/odom', 'map', rospy.Time(0))
+    tb1_pos[0] = trans1[0]
+    tb1_pos[1] = trans1[1]
+    # rospy.loginfo('tb1 position: [%.4f, %.4f]', tb1_pos[0], tb1_pos[1])
+
+    (trans2, rot2) = listener.lookupTransform('tb3_2/odom', 'map', rospy.Time(0))
+    tb2_pos[0] = trans2[0]
+    tb2_pos[1] = trans2[1]
+    # rospy.loginfo('tb2 position: [%.4f, %.4f]', tb2_pos[0], tb2_pos[1])
+
 
 
 if __name__=='__main__':
@@ -79,73 +171,14 @@ if __name__=='__main__':
     # initialization
     rospy.init_node('CBBA_node')
     rospy.loginfo("CBBA node started")
-    rospy.loginfo("\na: start receiving pnts \ns: finish receiving pnts\n"
-                  "d: distribute tasks    \nw: flush ")
-    # reg point receiver
-    point_receiver()
-    # reg keyboard input
-    user_input()
-
+    task_receiver()
     br=tf.TransformBroadcaster()
-
     rate=rospy.Rate(30)
     # main loop
     while not rospy.is_shutdown():
-
-        # marker for search positions published
-        pos_idx=0
-        marker_array=MarkerArray()
-        for pos in search_pos:
-            # visualization marker
-            marker=Marker()
-
-            marker.type=Marker.CUBE
-            marker.action=Marker.ADD
-            marker.header.frame_id="map"
-            marker.header.stamp=rospy.Time.now()
-            marker.id=pos_idx
-            marker.lifetime=rospy.Duration()
-            marker.ns='search_positions'
-            scale=1
-            marker.scale.x=scale ; marker.scale.y=scale; marker.scale.z=scale
-            marker.pose.orientation.w=1.0
-            marker.color.r=1; marker.color.a=1
-            marker.pose.position=pos
-
-            marker_array.markers.append(marker)
-            # transform
-            pos_name='sp'+str(pos_idx)
-            br.sendTransform((pos.x,pos.y,pos.z),(0,0,0,1),rospy.Time.now(),pos_name,'map')
-            pos_idx+=1
-
-
-        search_pos_marker_pub.publish(marker_array)
-
-        '''
-        (trans0, rot0) = listener.lookupTransform('tb3_0/odom', 'map', rospy.Time(0))
-        tb0_pos[0] = trans0[0]
-        tb0_pos[1] = trans0[1]
-        # rospy.loginfo('tb0 position: [%.4f, %.4f]', tb0_pos[0], tb0_pos[1])
-        (trans1, rot1) = listener.lookupTransform('tb3_1/odom', 'map', rospy.Time(0))
-        tb1_pos[0] = trans1[0]
-        tb1_pos[1] = trans1[1]
-        # rospy.loginfo('tb1 position: [%.4f, %.4f]', tb1_pos[0], tb1_pos[1])
-
-        (trans2, rot2) = listener.lookupTransform('tb3_2/odom', 'map', rospy.Time(0))
-        tb2_pos[0] = trans2[0]
-        tb2_pos[1] = trans2[1]
-        # rospy.loginfo('tb2 position: [%.4f, %.4f]', tb2_pos[0], tb2_pos[1])
-        '''
-        if isFinishing and isCalc:
-            isCalc = False # already calculating
-
+        # search position marker
+        marker_publish()
+        # CBBA solve ?
 
         rate.sleep()
-
-
-
-
-
-
-
 
